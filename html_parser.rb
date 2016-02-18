ed0b5584c75ddba9385fc7ef7ab11c05c6eba4bc
Phu Tang <phu.tanghong@2359media.com.vn>
2016-02-18 11:15:59 +0700

    override attr to get all attribute

--- html_parser.rb
+++ html_parser.rb
@@ -3,31 +3,32 @@ require 'nokogiri'
 
 module HtmlParser
   @html_content
+  @@attributes =[]
+
+  def self.included base
+    def base.attr *args
+      @@attributes.concat(args)
+      args.each do |attribute|
+        self.send(:attr_accessor, attribute)
+      end
+    end
+  end
 
 
   def updateContent(value)
     @html_content = Nokogiri::XML(value)
-    # temp = @html_content.css('div')[0]['id']
-    # puts (temp)
-    #
-    # temp  = @html_content.css('div').length
-    # puts (temp)
-
     set_data_to_variable
   end
 
-  def method_missing(method_id, *arguments, &block)
-    puts (method_id)
-  end
 
   def set_data_to_variable
-    for i in 0..@html_content.css('div').length-1
-      var_name = @html_content.css('div')[i]['id']
-      var_name = "@"+ var_name.to_s
-      value = @html_content.css('div')[i].text
-      instance_variable_set(var_name,value)
+    
+    @@attributes.each do |attribute|
+      var_name = "div#"+attribute.to_s
+      value = @html_content.css(var_name).text
+      instance_variable_set("@"+attribute.to_s, value)
+
     end
   end
-
 
 end
